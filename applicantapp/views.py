d44e10bedad24ff35e1fe6a4258c024ee6e31fda
Vladimir218 <kvv23@tut.by>
2023-01-31 23:12:31 +0300

    sprint_1 Добавлены HTML страницы вывода данных по отдельному резюме и соискателю

--- applicantapp/views.py
+++ applicantapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, TemplateView
 from applicantapp.models import *
 
@@ -23,4 +23,27 @@ class ResumeList(ListView):
         context = super().get_context_data(**kwargs)
         context["resume_list"] = Resumes.objects.all()
         print(context["resume_list"])
+        return context
+
+# Отдельное резюме
+class Resume(ListView):
+    model = Resumes
+    template_name = "applicantapp/resume.html"
+
+    def get_context_data(self, **kwargs):
+        profile = Resumes.objects.get(id = self.kwargs['resume_id'])
+        context = super().get_context_data(**kwargs)
+        context["resume"] = profile
+        context["skills"] = profile.skills.all()[:2]
+        return context
+
+# Отдельный соискатель
+class Applicant(ListView):
+    model = Applicants
+    template_name = "applicantapp/applicant.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["applicant"] = Applicants.objects.get(id = self.kwargs['applicant_id'])
         return context
