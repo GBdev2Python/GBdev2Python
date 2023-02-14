32b057230cc1bfd11cd9b527b907580d09b16192
oksanaust <okc040281@yandex.ru>
2023-02-14 20:19:13 +0300

    Change start-page

--- hhapp/views.py
+++ hhapp/views.py
@@ -6,6 +6,17 @@ from django.views.generic import TemplateView
 class MainPageView(TemplateView):
     template_name = "hhapp/index.html"
 
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        return context
+
 
 class JobsListView(TemplateView):
     template_name = "hhapp/jobs.html"
