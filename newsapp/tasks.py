e0ab33ff485d0d73bb6242b8ea013500dad12dff
StanislavVNikitin <stasvnikitin@gmail.com>
2023-02-22 08:54:37 +0300

    Vacancy insert response

--- newsapp/tasks.py
+++ newsapp/tasks.py
@@ -9,10 +9,10 @@ import sys
 import django
 import requests
 
-# if "zoneinfo" in sys.modules:
-#import zoneinfo
-# elif "backports.zoneinfo" in sys.modules:
-from backports import zoneinfo
+#if "zoneinfo" in sys.modules:
+    import zoneinfo
+#elif "backports.zoneinfo" in sys.modules:
+#    from backports import zoneinfo
 
 from datetime import datetime
 from http import HTTPStatus
